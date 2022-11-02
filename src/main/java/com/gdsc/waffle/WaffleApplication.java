22809882448f131678b6ca90be34f63264d669c8
Jung <anes53027@gmail.com>
2022-11-02 10:16:50 +0900

    테스트 위한 데이터 생성

--- src/main/java/com/gdsc/waffle/WaffleApplication.java
+++ src/main/java/com/gdsc/waffle/WaffleApplication.java
@@ -1,7 +1,14 @@
 package com.gdsc.waffle;
 
+import com.gdsc.waffle.domain.TodoEntity;
+import com.gdsc.waffle.repository.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class WaffleApplication {
@@ -10,4 +17,13 @@ public class WaffleApplication {
         SpringApplication.run(WaffleApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(TodoRepository todoRepository) {
+        return (args) -> IntStream.rangeClosed(1, 10).forEach(index -> todoRepository.save(TodoEntity.builder()
+                .content("오늘 할 일" + index)
+                .createdDateTime(LocalDateTime.now())
+                .status(false)
+                .build())
+        );
+    }
 }
