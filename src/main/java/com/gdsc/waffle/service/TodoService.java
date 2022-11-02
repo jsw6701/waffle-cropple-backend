f706a20471dd6124120a53af5a3e46fb66c7bbbf
Jung <anes53027@gmail.com>
2022-11-02 10:05:09 +0900

    Service Interface CR(U)D

--- src/main/java/com/gdsc/waffle/service/TodoService.java
+++ src/main/java/com/gdsc/waffle/service/TodoService.java
@@ -1,59 +1,21 @@
 package com.gdsc.waffle.service;
 
 import com.gdsc.waffle.domain.TodoEntity;
-import com.gdsc.waffle.dto.TodoRequest;
-import com.gdsc.waffle.repository.TodoRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
-@Service
-@AllArgsConstructor
-public class TodoService {
-    private final TodoRepository todoRepository;
+public interface TodoService {
 
-    public TodoEntity add(TodoRequest request) {
-        TodoEntity todoEntity = new TodoEntity();
 
-        todoEntity.setContent(request.getContent());
-        todoEntity.setStatus(request.getStatus());
+    TodoEntity findById(Integer id);
 
-        TodoEntity result = this.todoRepository.save(todoEntity);
+    void addTodoList(TodoEntity todoEntity);
 
-        return result;
-    }
+    TodoEntity updateTodoList(TodoEntity todoEntity);
 
-    public TodoEntity searchById(Integer id) {
-        return this.todoRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
+    void deleteTodoList(Integer id);
 
-    public List<TodoEntity> searchAll() {
-        return this.todoRepository.findAll();
-    }
+    List<TodoEntity> findTodoList(Sort sort);
 
-    public TodoEntity updateById(Integer id, TodoRequest todoDTO) {
-        TodoEntity todoEntity = this.searchById(id);
-
-        if (todoDTO.getContent() != null) {
-            todoEntity.setContent(todoDTO.getContent());
-        }
-
-        if (todoDTO.getStatus() != null) {
-            todoEntity.setStatus(todoDTO.getStatus());
-        }
-
-        return this.todoRepository.save(todoEntity);
-    }
-
-    public void deleteById(Integer id) {
-        this.todoRepository.deleteById(id);
-    }
-
-    public void deleteAll() {
-        this.todoRepository.deleteAll();
-    }
 }
