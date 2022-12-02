66cdd2cdcb06689a735a80fb4bf86419513ff22e
Jung <anes53027@gmail.com>
2022-12-02 15:08:47 +0900

    자동 데이터 생성 중지

--- src/main/java/com/gdsc/waffle/WaffleApplication.java
+++ src/main/java/com/gdsc/waffle/WaffleApplication.java
@@ -1,17 +1,8 @@
 package com.gdsc.waffle;
 
-import com.gdsc.waffle.domain.CategoryEntity;
-import com.gdsc.waffle.domain.TodoEntity;
-import com.gdsc.waffle.repository.CategoryRepository;
-import com.gdsc.waffle.repository.TodoRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 @SpringBootApplication
 public class WaffleApplication {
@@ -19,23 +10,4 @@ public class WaffleApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaffleApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner runner(TodoRepository todoRepository) {
-        return (args) -> IntStream.rangeClosed(1, 10).forEach(index -> todoRepository.save(TodoEntity.builder()
-                .content("오늘 할 일" + index)
-                .createdDateTime(LocalDateTime.now())
-                .status(false)
-                .build())
-        );
-    }
-
-    /*@Bean
-    public CommandLineRunner runner(CategoryRepository categoryRepository){
-        return (args) -> IntStream.rangeClosed(1, 10).forEach(index -> categoryRepository.save(CategoryEntity.builder()
-                .categoryId((long) index)
-                .categoryTitle("카테고리 제목 : " + index)
-                .build())
-        );
-    }*/
 }
